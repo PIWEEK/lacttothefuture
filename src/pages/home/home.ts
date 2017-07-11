55807dd20054556b06b57e15c25c6d97bde10352
Pablo Alba <pablo.alba@kaleidos.net>
2017-07-11 12:18:49 +0200

    Some real data

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -5,6 +5,8 @@ import { SleepPage} from "../sleep/sleep"
 import { DoctorPage} from "../doctor/doctor"
 
 import { RepositoryProvider } from "../../providers/repository/repository";
+import { Observable, Subscription } from 'rxjs/Rx';
+
 
 
 @Component({
@@ -13,11 +15,42 @@ import { RepositoryProvider } from "../../providers/repository/repository";
   providers: [RepositoryProvider]
 })
 export class HomePage {
-  currentBaby: any;
+  private currentBaby: any;
+  private $counter: Observable<number>;
+  private subscription: Subscription;
+  private hoursNextEat: string;
+  private hoursNextSleep: string;
+  private hoursSleeping: string;
+  private daysNextDoctor: string;
+  private dateNextDoctor: string;
+
+  private debug: string;
+
 
   constructor(public navCtrl: NavController, public repository: RepositoryProvider) {
     this.currentBaby = repository.getCurrentBaby();
+    this.debug = "";
   }
+
+  ngOnInit() {
+    console.log('ngOnInit');
+    this.updateInfo();
+      //every 30 seconds
+      this.$counter = Observable.interval(30000).map((x) => {
+          return x;
+      });
+
+      this.subscription = this.$counter.subscribe((x) => {
+          this.updateInfo();
+      });
+  }
+
+  ngOnDestroy(): void {
+      this.subscription.unsubscribe();
+  }
+
+
+
 
   itemTapped(item) {
     if (item === 'eat') {
@@ -28,6 +61,86 @@ export class HomePage {
       this.navCtrl.push(DoctorPage, {})
     }
 
+  }
+
+  updateInfo() {
+    var millis
+          var hours
+          var mins
+          var minsPad
+
+          var today = new Date();
+          var now = today.getTime();
+
+          // Eat
+          millis = this.currentBaby.nextEat.prediction - now;
+          if (millis > 0){
+            hours = Math.floor(millis / 3600000);
+            mins = Math.floor(((millis - (hours * 3600000)) / 60000));
+            minsPad = ("0" + mins).slice(-2)
+          } else {
+            hours = 0
+            minsPad = "00"
+          }
+          this.hoursNextEat = hours + "h "+minsPad+"min";
+
+
+          // Sleep prediction
+          millis = this.currentBaby.nextSleep.prediction - now;
+          if (millis > 0){
+            hours = Math.floor(millis / 3600000);
+            mins = Math.floor(((millis - (hours * 3600000)) / 60000));
+            minsPad = ("0" + mins).slice(-2)
+          } else {
+            hours = 0
+            minsPad = "00"
+          }
+          this.hoursNextSleep = hours + "h "+minsPad+"min";
+
+          // Sleeping
+          millis = now - this.currentBaby.nextSleep.timestamp;
+          if (millis > 0){
+            hours = Math.floor(millis / 3600000);
+            mins = Math.floor(((millis - (hours * 3600000)) / 60000));
+            minsPad = ("0" + mins).slice(-2)
+          } else {
+            hours = 0
+            minsPad = "00"
+          }
+          this.hoursSleeping = hours + "h "+minsPad+"min";
+
+
+          // Doctor
+          var nextDoctor = "0h";
+          var appointment = new Date(this.currentBaby.nextDoctor.timestamp);
+
+          this.dateNextDoctor = appointment.getDate() +
+            "/" +("0"+(appointment.getMonth()+1)).slice(-2) +
+            "/" + appointment.getFullYear()
+            + " " + ("0"+appointment.getHours()).slice(-2)
+            + ":" + ("0"+appointment.getMinutes()).slice(-2);
+
+          millis = this.currentBaby.nextDoctor.timestamp - now;
+          if (millis >0) {
+            var days = Math.floor(millis / 86400000);
+            if (days > 1){
+                nextDoctor = days +" días"
+            } else if (days == 1) {
+                nextDoctor = "Mañana"
+            } else {
+              if (today.getDate() != appointment.getDate()){
+                //Less than 24h, but tomorrow
+                nextDoctor = "Mañana"
+              } else {
+                hours = Math.floor(millis / 3600000);
+                mins = Math.floor(((millis - (hours * 3600000)) / 60000));
+                minsPad = ("0" + mins).slice(-2)
+                nextDoctor = hours + "h "+minsPad+"min";
+              }
+            }
+          }
+
+          this.daysNextDoctor = nextDoctor;
   }
 
 }
--- src/providers/repository/repository.ts
+++ src/providers/repository/repository.ts
@@ -16,7 +16,29 @@ export class RepositoryProvider {
   constructor() {
 
     this.babiesList = [
-      {name:'Diego'},
+      {
+        name:'Diego',
+        sex: 'boy',
+        nextEat: {
+          prediction: new Date().getTime() + 120 * 60000,
+          last: new Date().getTime() - 122 * 60000,
+          totalToday: '2h 21min',
+          mealsToday: 3,
+          happy: 5,
+          nextType: 'PECHOD'
+        },
+        nextSleep: {
+          prediction: new Date().getTime()  + 240 * 60000,
+          timestamp: new Date().getTime()  - 37 * 60000,
+          status: "DESPIERTO",
+          happy: 4,
+          daytime: 'afternoon'
+        },
+        nextDoctor: {
+          timestamp: new Date().getTime()  + 1.2 * 24 * 60 * 60000,
+          notes: "Cita con el pediatra por fiebre y vómitos. Malestar general, sobre todo de noche, que no hay quién le duerma."
+        }
+      },
       {name:'Pepe'}
     ]
 
