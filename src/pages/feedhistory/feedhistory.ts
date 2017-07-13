82c6e65080682422b4539970f5d8dcc2bc91399f
Pablo Alba <pablo.alba@kaleidos.net>
2017-07-13 13:13:50 +0200

    Feed history

--- src/pages/feedhistory/feedhistory.ts
+++ src/pages/feedhistory/feedhistory.ts
@@ -1,12 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { RepositoryProvider } from "../../providers/repository/repository";
 
-/**
- * Generated class for the FeedhistoryPage page.
- *
- * See http://ionicframework.com/docs/components/#navigation for more info
- * on Ionic pages and navigation.
- */
 @IonicPage()
 @Component({
   selector: 'page-feedhistory',
@@ -14,11 +9,32 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class FeedhistoryPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public repository: RepositoryProvider) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad FeedhistoryPage');
+  }
+
+  public selectFeedIcon(entry){
+    if (entry.type == 'breast') {
+      return 'both_breast';       
+    } else if (entry.type == 'solid') {
+      return 'eat_solid';            
+    } else{
+      return 'eat_bottle';
+    } 
+  }
+
+  public calcMinutes(millis){
+    if (millis && millis > 0){
+        var mins = Math.floor(millis / 60000);
+        var sec = Math.floor(((millis - (mins * 60000)) / 1000));
+        var secPad = ("0" + sec).slice(-2)
+        return mins + "m "+secPad+"s";
+      } else {
+        return "0m 0s";
+      }
   }
 
 }
